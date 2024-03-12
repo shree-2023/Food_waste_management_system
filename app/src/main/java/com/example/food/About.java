8c3984c961227262b9cbd74fc6485f3d9e186b4b
shree-2023 <132129359+shree-2023@users.noreply.github.com>
2024-03-12 12:38:54 +0530

    FWmS_updated3

--- app/src/main/java/com/example/food/About.java
+++ app/src/main/java/com/example/food/About.java
@@ -1,14 +1,38 @@
 package com.example.food;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class About extends AppCompatActivity {
+public class Aboutus extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+
+        // Set the custom action bar view
+        TextView backbtn;
+        backbtn=findViewById(R.id.actionBarIcon);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Aboutus.this, Home.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
--- app/src/main/java/com/example/food/MainActivity.java
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -1,26 +1,35 @@
 package com.example.food;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-Button b;
-    @SuppressLint("MissingInflatedId")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // on below line we are configuring our window to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
-        b=(Button) findViewById(R.id.button2);
-        b.setOnClickListener(new View.OnClickListener (){
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Home.class));
+            public void run() {
+                // creating a new intent
+//                Intent i = new Intent(MainActivity.this, SendVoicemsg.class);
+                Intent i = new Intent(MainActivity.this, Home.class);
+                // starting a new activity.
+                startActivity(i);
+                finish();
             }
-        });
+        }, 2000);
+
     }
 }
